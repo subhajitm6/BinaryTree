99806c97051fa3f6cea26c3225d82975e454e611
Subhajit Manna <subhajitmanna0045@gmail.com>
2023-06-29 21:17:02 +0530

    initial commit

---
+++ TreeBuilding.java
@@ -0,0 +1,72 @@
+public class TreeBuilding {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int x) {
+            data = x;
+            left = null;
+            right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void postorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree bt = new BinaryTree();
+        Node root = bt.buildTree(nodes);
+
+        System.out.println("preorder");
+        preorder(root);
+        System.out.println();
+        System.out.println("inorder");
+        inorder(root);
+        System.out.println();
+        System.out.println("postorder");
+        postorder(root);
+        System.out.println();
+    }
+}
